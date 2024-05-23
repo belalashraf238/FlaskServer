7dc9de558684c8b843c776240e2e5e1dabd06943
belalashraf238 <59982142+belalashraf238@users.noreply.github.com>
2024-05-23 21:17:16 +0300

    Add files via upload

--- Project/app.py
+++ Project/app.py
@@ -1,251 +1,254 @@
 from sqlalchemy import create_engine
 from sqlalchemy_utils import create_database, database_exists
-from flask_restful import Api, Resource, reqparse
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from datetime import datetime
 from flask import Flask, jsonify, request
-from models import db, Status, SMS, Calls, User
+from models import db, Device, RI, WiFi, GSM, SMS, Call, User
+from flask_httpauth import HTTPBasicAuth
+import random
+import string
 
-engine = create_engine('mysql+pymysql://belal:17242524@localhost/MobileTestData')
+engine = create_engine('mysql+pymysql://root:@localhost/MockDataBase1')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = engine.url
 db.init_app(app)
-api = Api(app)
-jwt = JWTManager(app)
-app.config['SECRET_KEY'] = 'zeyad'
-app.config['JWT_SECRET_KEY'] = 'zeyad'
+auth = HTTPBasicAuth()
+
+app.config['SECRET_KEY'] = 'secret-key'
+
 if database_exists(engine.url):
     print("Database already exists")
 else:
     if not create_database(engine.url):
         print('Database "mobile" created successfully.')
-    app.config['SQLALCHEMY_DATABASE_URI'] = engine.url
+        app.config['SQLALCHEMY_DATABASE_URI'] = engine.url
     with app.app_context():
         db.create_all()
 
 
-def authenticate(username, password):
-    user = User.query.filter_by(username=username, password=password).first()
-    return user
+@auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username=username).first()
+    if user and user.password == password:
+        return user
 
 
-# User API Endpoints
-class UserRegister(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('username', type=str, required=True)
-        parser.add_argument('password', type=str, required=True)
-        args = parser.parse_args()
-
-        user = User(username=args['username'], password=args['password'])
-        db.session.add(user)
-        db.session.commit()
-        return {'message': 'User registered successfully'}, 201
+@app.route('/')
+def index():
+    return 'Hello World!'
 
 
-class UserLogin(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('username', type=str, required=True)
-        parser.add_argument('password', type=str, required=True)
-        args = parser.parse_args()
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    if 'username' not in data :
+        return jsonify({'message': 'Username is required'}), 400
 
-        user = authenticate(args['username'], args['password'])
-        if not user:
-            return {'message': 'Invalid credentials'}, 401
+    username = data['username']
+    # Generate a random password
+    password_length = 8
+    password_characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(password_characters) for _ in range(password_length))
 
-        access_token = create_access_token(identity=user.User_id)
-        return {'access_token': access_token}, 200
+    # Create a new user
+    new_user = User(username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
 
+    new_wifi = WiFi(user_id=new_user.id)
+    db.session.add(new_wifi)
 
-class UserData(Resource):
-    @jwt_required()
-    def get(self):
-        current_user = get_jwt_identity()
-        user = User.query.get(current_user)
-        return {'username': user.username
-            , 'User_id': user.User_id}, 200
+    new_gsm = GSM(user_id=new_user.id)
+    db.session.add(new_gsm)
 
-    @jwt_required()
-    def put(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('username', type=str)
-        parser.add_argument('password', type=str)
-        args = parser.parse_args()
+    new_device = Device(user_id=new_user.id)
+    db.session.add(new_device)
 
-        current_user = get_jwt_identity()
-        user = User.query.get(current_user)
+    new_ri = RI(user_id=new_user.id)
+    db.session.add(new_ri)
 
-        if args['username']:
-            user.username = args['username']
-        if args['password']:
-            user.password = args['password']
-
-        db.session.commit()
-        return {'message': 'User data updated successfully'}, 200
+    new_sms = SMS(user_id=new_user.id)
+    db.session.add(new_sms)
+    new_call = Call(user_id=new_user.id)
+    db.session.add(new_call)
+    db.session.commit()
 
 
-status_parser = reqparse.RequestParser()
-status_parser.add_argument('signal_strength', type=int)
-status_parser.add_argument('signal_level_description', type=str)
-status_parser.add_argument('units', type=str)
-status_parser.add_argument('reachable_status', type=bool)
-status_parser.add_argument('roaming_status', type=bool)
-status_parser.add_argument('mcc', type=str)
-status_parser.add_argument('mnc', type=str)
-status_parser.add_argument('network_operator_name', type=str)
-status_parser.add_argument('phone_number', type=str)
-status_parser.add_argument('network_country_iso', type=str)
-status_parser.add_argument('charging', type=bool)
-status_parser.add_argument('battery_level', type=str)
-status_parser.add_argument('access_wifi_state', type=bool)
-status_parser.add_argument('access_location', type=bool)
-status_parser.add_argument('access_phone_state', type=bool)
-status_parser.add_argument('access_phone_numbers', type=bool)
-status_parser.add_argument('access_phone_calls', type=bool)
-status_parser.add_argument('access_contacts', type=bool)
-status_parser.add_argument('access_sms', type=bool)
-status_parser.add_argument('access_call_interception', type=bool)
-status_parser.add_argument('app_running_since', type=str)
-status_parser.add_argument('calls_received', type=int)
-status_parser.add_argument('last_call_received', type=str)
-status_parser.add_argument('user_id', type=int)
-
-sms_parser = reqparse.RequestParser()
-sms_parser.add_argument('phone_number', type=str)
-sms_parser.add_argument('sender_id', type=str)
-sms_parser.add_argument('sms_content', type=str)
-sms_parser.add_argument('time_received', type=str)
-sms_parser.add_argument('user_id', type=int)
-
-calls_parser = reqparse.RequestParser()
-calls_parser.add_argument('phone_number', type=str)
-calls_parser.add_argument('caller_id', type=str)
-calls_parser.add_argument('time_received', type=str)
-calls_parser.add_argument('user_id', type=int)
+    return jsonify({"data":{
+        "username": username,
+        "password": password,
+    }}), 201
 
 
-class StatusListResource(Resource):
-    def get(self, user_id):
-        statuses = Status.query.filter_by(user_id=user_id).all()
-        return [{'status_id': status.status_id, 'signal_strength': status.signal_strength} for status in statuses]
-
-    def post(self, user_id):
-
-        args = status_parser.parse_args()
-        args['user_id'] = user_id
-        new_status = Status(**args)
-        db.session.add(new_status)
-        db.session.commit()
-        return {'message': 'Status added successfully'}, 201
+@app.route('/login', methods=['POST'])
+@auth.login_required
+def login():
+    
+    
+   
+    return jsonify({"message": f"Hello, {auth.current_user().username}!"}), 200
 
 
-class StatusResource(Resource):
-    def get(self, user_id, status_id):
-        status = Status.query.filter_by(user_id=user_id, status_id=status_id).first_or_404()
-        return {'status_id': status.status_id, 'signal_strength': status.signal_strength}
+@app.route('/sms', methods=['PUT'])
+@auth.login_required
+def put_sms():
+    data = request.json.get('data', [])
+    current_user = auth.current_user()
+    if not data:
+        return jsonify(message="No SMS data provided"), 400
 
-    def delete(self, user_id, status_id):
-        status = Status.query.filter_by(user_id=user_id, status_id=status_id).first_or_404()
-        db.session.delete(status)
-        db.session.commit()
-        return {'message': 'Status deleted successfully'}, 200
+    for sms_data in data:
+        phone_number = sms_data.get('phone_number')
+        sender_id = sms_data.get('sender_id')
+        sms_content = sms_data.get('sms_content')
+        time_received_str = sms_data.get('time_received')
 
-    def put(self, user_id, status_id):
-        status = Status.query.filter_by(user_id=user_id, status_id=status_id).first_or_404()
-        args = status_parser.parse_args()
-        for key, value in args.items():
-            if value is not None:
-                setattr(status, key, value)
-        db.session.commit()
-        return {'message': 'Status updated successfully'}, 200
+        try:
+            time_received = datetime.strptime(time_received_str, "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            return jsonify(message="Invalid time format"), 400
 
+        # Assuming you have a current_user variable representing the authenticated user
 
-class SMSListResource(Resource):
-    def get(self, user_id):
-        sms_list = SMS.query.filter_by(user_id=user_id).all()
-        return [{'sms_id': sms.sms_id, 'phone_number': sms.phone_number, 'sender_id': sms.sender_id,
-                 'sms_content': sms.sms_content, 'time_received': sms.time_received} for sms in sms_list]
+        # Check if an SMS with the same user ID and phone number exists
+        existing_sms = SMS.query.filter_by(user_id=current_user.id).first()
 
-    def post(self, user_id):
-        args = sms_parser.parse_args()
-        args['user_id'] = user_id
-        new_sms = SMS(**args)
-        db.session.add(new_sms)
-        db.session.commit()
-        return {'message': 'SMS added successfully'}, 201
+        if existing_sms:
+            # Update existing SMS
+            existing_sms.sender_id = sender_id
+            existing_sms.sms_content = sms_content
+            existing_sms.time_received = time_received
+            existing_sms.phone_number = phone_number
+        else:
+            # Create a new SMS entry
+            new_sms = SMS(
+                phone_number=phone_number,
+                sender_id=sender_id,
+                sms_content=sms_content,
+                time_received=time_received,
+                user_id=current_user.id
+            )
+            db.session.add(new_sms)
 
+    db.session.commit()
 
-class SMSResource(Resource):
-    def get(self, user_id, sms_id):
-        sms = SMS.query.filter_by(user_id=user_id, sms_id=sms_id).first_or_404()
-        return {'sms_id': sms.sms_id, 'phone_number': sms.phone_number, 'sender_id': sms.sender_id,
-                'sms_content': sms.sms_content, 'time_received': sms.time_received}
-
-    def delete(self, user_id, sms_id):
-        sms = SMS.query.filter_by(user_id=user_id, sms_id=sms_id).first_or_404()
-        db.session.delete(sms)
-        db.session.commit()
-        return {'message': 'SMS deleted successfully'}, 200
-
-    def put(self, user_id, sms_id):
-        sms = SMS.query.filter_by(user_id=user_id, sms_id=sms_id).first_or_404()
-        args = sms_parser.parse_args()
-        for key, value in args.items():
-            if value is not None:
-                setattr(sms, key, value)
-        db.session.commit()
-        return {'message': 'SMS updated successfully'}, 200
+    return jsonify(message="SMS data processed successfully"), 200
 
 
-class CallsListResource(Resource):
-    def get(self, user_id):
-        calls_list = Calls.query.filter_by(user_id=user_id).all()
-        return [{'calls_id': call.calls_id, 'phone_number': call.phone_number, 'caller_id': call.caller_id,
-                 'time_received': call.time_received} for call in calls_list]
+@app.route('/call', methods=['PUT'])
+@auth.login_required
+def put_call():
+    data = request.json.get('data', [])
+    current_user = auth.current_user()
+    if not data:
+        return jsonify(message="No call data provided"), 400
 
-    def post(self, user_id):
-        args = calls_parser.parse_args()
-        args['user_id'] = user_id
-        new_call = Calls(**args)
+    for call_data in data:
+        phone_number = call_data.get('phone_number')
+        caller_id = call_data.get('caller_id')
+        time_received_str = call_data.get('time_received')
+
+        try:
+            time_received = datetime.strptime(time_received_str, "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            return jsonify(message="Invalid time format"), 400
+
+        # Assuming you have a current_user variable representing the authenticated user
+        existing_call = Call.query.filter_by(user_id=current_user.id).first()
+
+        if existing_call:
+            # Update existing SMS
+            existing_call.phone_number = phone_number
+            existing_call.caller_id = caller_id
+            existing_call.time_received_str = time_received
+
+        # Create a new Call entry
+        new_call = Call(
+            phone_number=phone_number,
+            caller_id=caller_id,
+            time_received=time_received,
+            user_id=current_user.id
+        )
         db.session.add(new_call)
-        db.session.commit()
-        return {'message': 'Call added successfully'}, 201
+
+    db.session.commit()
+
+    return jsonify(message="Call data processed successfully"), 200
 
 
-class CallsResource(Resource):
-    def get(self, user_id, call_id):
-        call = Calls.query.filter_by(user_id=user_id, calls_id=call_id).first_or_404()
-        return {'calls_id': call.calls_id, 'phone_number': call.phone_number, 'caller_id': call.caller_id,
-                'time_received': call.time_received}
+@app.route('/status', methods=['PUT'])
+@auth.login_required
+def put_data():
+    current_user = auth.current_user()  # Extract user from authentication
+    data = request.json.get('data', {})
 
-    def delete(self, user_id, call_id):
-        call = Calls.query.filter_by(user_id=user_id, calls_id=call_id).first_or_404()
-        db.session.delete(call)
-        db.session.commit()
-        return {'message': 'Call deleted successfully'}, 200
+    # Update WiFi model
+    wifi_data = data.get('WiFi', {})
+    update_wifi(current_user.id, wifi_data)
 
-    def put(self, user_id, call_id):
-        call = Calls.query.filter_by(user_id=user_id, calls_id=call_id).first_or_404()
-        args = calls_parser.parse_args()
-        for key, value in args.items():
-            if value is not None:
-                setattr(call, key, value)
-        db.session.commit()
-        return {'message': 'Call updated successfully'}, 200
+    # Update GSM model
+    gsm_data = data.get('GSM', {})
+    update_gsm(current_user.id, gsm_data)
+
+    # Update Device model
+    device_data = data.get('Device', {})
+    update_device(current_user.id, device_data)
+
+    # Update RI model
+    ri_data = data.get('RI', {})
+    update_ri(current_user.id, ri_data)
+
+    db.session.commit()
+
+    return jsonify(message="Data updated successfully"), 200
 
 
-api.add_resource(StatusListResource, '/users/<int:user_id>/statuses')
-api.add_resource(StatusResource, '/users/<int:user_id>/statuses/<int:status_id>')
-api.add_resource(UserRegister, '/register')
-api.add_resource(UserLogin, '/login')
-api.add_resource(UserData, '/user')
-api.add_resource(SMSListResource, '/users/<int:user_id>/sms')
-api.add_resource(SMSResource, '/users/<int:user_id>/sms/<int:sms_id>')
-api.add_resource(CallsListResource, '/users/<int:user_id>/calls')
-api.add_resource(CallsResource, '/users/<int:user_id>/calls/<int:call_id>')
+def update_wifi(user_id, wifi_data):
+    wifi = WiFi.query.filter_by(user_id=user_id).first()
+    if wifi:
+        wifi.signal_strength = wifi_data.get('signalStrength', wifi.signal_strength)
+        wifi.signal_level_description = wifi_data.get('signalLevelDescription', wifi.signal_level_description)
+        wifi.units = wifi_data.get('units', wifi.units)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+def update_gsm(user_id, gsm_data):
+    gsm = GSM.query.filter_by(user_id=user_id).first()
+    if gsm:
+        gsm.signal_strength = gsm_data.get('signalStrength', gsm.signal_strength)
+        gsm.signal_level_description = gsm_data.get('signalLevelDescription', gsm.signal_level_description)
+        gsm.units = gsm_data.get('units', gsm.units)
+        gsm.reachable_status = gsm_data.get('reachableStatus', gsm.reachable_status)
+        gsm.roaming_status = gsm_data.get('roamingStatus', gsm.roaming_status)
+        gsm.mcc = gsm_data.get('MCC', gsm.mcc)
+        gsm.mnc = gsm_data.get('MNC', gsm.mnc)
+        gsm.network_operator_name = gsm_data.get('networkOperatorName', gsm.network_operator_name)
+        gsm.phone_number = gsm_data.get('phoneNumber', gsm.phone_number)
+        gsm.network_country_iso = gsm_data.get('networkCountryISO', gsm.network_country_iso)
+
+
+def update_device(user_id, device_data):
+    device = Device.query.filter_by(user_id=user_id).first()
+    if device:
+        device.charging = device_data.get('charging', device.charging)
+        device.battery_level = device_data.get('batteryLevel', device.battery_level)
+        device.access_wifi_state = device_data.get('access_wifi_state', device.access_wifi_state)
+        device.access_location = device_data.get('access_location', device.access_location)
+        device.access_phone_state = device_data.get('access_phone_state', device.access_phone_state)
+        device.access_phone_numbers = device_data.get('access_phone_numbers', device.access_phone_numbers)
+        device.access_phone_calls = device_data.get('access_phone_calls', device.access_phone_calls)
+        device.access_contacts = device_data.get('access_contacts', device.access_contacts)
+        device.access_sms = device_data.get('access_sms', device.access_sms)
+        device.access_call_interception = device_data.get('access_call_interception', device.access_call_interception)
+
+
+def update_ri(user_id, ri_data):
+    ri = RI.query.filter_by(user_id=user_id).first()
+    if ri:
+        ri.app_running_since = datetime.strptime(ri_data.get('appRunningSince', ri.app_running_since),
+                                                 "%Y-%m-%d %H:%M:%S")
+        ri.calls_received = ri_data.get('callsReceived', ri.calls_received)
+        ri.last_call_received = datetime.strptime(ri_data.get('lastCallReceived', ri.last_call_received),
+                                                  "%Y-%m-%d %H:%M:%S")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Project/models.py
+++ Project/models.py
@@ -1,61 +1,78 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
+from datetime import datetime
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
-    User_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
+    # Relationships
+    wifis = db.relationship('WiFi', backref='user', cascade="all, delete-orphan", uselist=False)
+    gsms = db.relationship('GSM', backref='user', cascade="all, delete-orphan", uselist=False)
+    devices = db.relationship('Device', backref='user', cascade="all, delete-orphan", uselist=False)
+    ris = db.relationship('RI', backref='user', cascade="all, delete-orphan", uselist=False)
+    sms = db.relationship('SMS', backref='user', cascade="all, delete-orphan", uselist=False)
+    calls = db.relationship('Call', backref='user', cascade="all, delete-orphan", uselist=False)
 
-class Status(db.Model):
-    status_id = db.Column(db.Integer, primary_key=True)
-    signal_strength = db.Column(db.Integer)
-    signal_level_description = db.Column(db.String(50))
-    units = db.Column(db.String(10))
-    reachable_status = db.Column(db.Boolean)
-    roaming_status = db.Column(db.Boolean)
-    mcc = db.Column(db.String(10))
-    mnc = db.Column(db.String(10))
-    network_operator_name = db.Column(db.String(100))
-    phone_number = db.Column(db.String(20))
-    network_country_iso = db.Column(db.String(2))
-    charging = db.Column(db.Boolean)
-    battery_level = db.Column(db.String(10))
-    access_wifi_state = db.Column(db.Boolean)
-    access_location = db.Column(db.Boolean)
-    access_phone_state = db.Column(db.Boolean)
-    access_phone_numbers = db.Column(db.Boolean)
-    access_phone_calls = db.Column(db.Boolean)
-    access_contacts = db.Column(db.Boolean)
-    access_sms = db.Column(db.Boolean)
-    access_call_interception = db.Column(db.Boolean)
-    app_running_since = db.Column(db.DateTime)
-    calls_received = db.Column(db.Integer)
-    last_call_received = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.User_id'), nullable=False)
-    user = relationship('User', backref='statuses')
+class WiFi(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    signal_strength = db.Column(db.Integer, nullable=False, default=0)
+    signal_level_description = db.Column(db.String(50), nullable=False, default='No Signal')
+    units = db.Column(db.String(10), nullable=False, default='dBm')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
+class GSM(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    signal_strength = db.Column(db.Integer, nullable=False, default=0)
+    signal_level_description = db.Column(db.String(50), nullable=False, default='No Signal')
+    units = db.Column(db.String(10), nullable=False, default='dBm')
+    reachable_status = db.Column(db.Boolean, nullable=False, default=False)
+    roaming_status = db.Column(db.Boolean, nullable=False, default=False)
+    mcc = db.Column(db.String(10), nullable=False, default='000')
+    mnc = db.Column(db.String(10), nullable=False, default='000')
+    network_operator_name = db.Column(db.String(100), nullable=False, default='Unknown')
+    phone_number = db.Column(db.String(20), nullable=False, default='0000000000')
+    network_country_iso = db.Column(db.String(10), nullable=False, default='US')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    charging = db.Column(db.Boolean, nullable=False, default=False)
+    battery_level = db.Column(db.String(10), nullable=False, default='0%')
+    access_wifi_state = db.Column(db.Boolean, nullable=False, default=False)
+    access_location = db.Column(db.Boolean, nullable=False, default=False)
+    access_phone_state = db.Column(db.Boolean, nullable=False, default=False)
+    access_phone_numbers = db.Column(db.Boolean, nullable=False, default=False)
+    access_phone_calls = db.Column(db.Boolean, nullable=False, default=False)
+    access_contacts = db.Column(db.Boolean, nullable=False, default=False)
+    access_sms = db.Column(db.Boolean, nullable=False, default=False)
+    access_call_interception = db.Column(db.Boolean, nullable=False, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class RI(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    app_running_since = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    calls_received = db.Column(db.Integer, nullable=False, default=0)
+    last_call_received = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class SMS(db.Model):
-    sms_id = db.Column(db.Integer, primary_key=True)
-    phone_number = db.Column(db.String(20))
-    sender_id = db.Column(db.String(100))
-    sms_content = db.Column(db.Text)
-    time_received = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.User_id'), nullable=False)
-    user = relationship('User', backref='sms')
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(20), default='0000000000')
+    sender_id = db.Column(db.String(100), default='0000000000')
+    sms_content = db.Column(db.Text, default='No content')
+    time_received = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-
-class Calls(db.Model):
-    calls_id = db.Column(db.Integer, primary_key=True)
-    phone_number = db.Column(db.String(20))
-    caller_id = db.Column(db.String(100))
-    time_received = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.User_id'), nullable=False)
-    user = relationship('User', backref='calls')
-
+class Call(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(20), default='0000000000')
+    caller_id = db.Column(db.String(100), default='0000000000')
+    time_received = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 # Define your database models here
 # Define the models for your tables
